fd6549c9c50bc098b60f01e9ed13fed3d04949b2
Eric Liu <ericyl.us@gmail.com>
2024-12-24 09:49:12 -0800

    chore(deps-dev): upgrade to Svelte 5

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,8 +1,7 @@
+import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 
-const app = new App({
+mount(App, {
   target: document.getElementById("app")!,
 });
-
-export default app;

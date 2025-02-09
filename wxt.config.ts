c4fd842d9354c0aa89e1203b002785db5d7c3579
Eric Liu <ericyl.us@gmail.com>
2025-02-09 09:44:25 -0800

    chore: use Svelte 5 syntax

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "wxt";
 
-// See https://wxt.dev/api/config.html
+// https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: "src",
   modules: ["@wxt-dev/module-svelte"],

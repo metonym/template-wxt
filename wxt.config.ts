4ee22cf407c0feb9f5839e7c6921ff593eb9af1d
Eric Liu <ericyl.us@gmail.com>
2025-02-09 10:28:33 -0800

    chore: use path alias

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   srcDir: "src",
   modules: ["@wxt-dev/module-svelte"],
+  alias: {
+    "@": "./src",
+  },
   vite: () => ({
     plugins: [tailwindcss()],
   }),
